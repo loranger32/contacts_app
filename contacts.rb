086aa86382a85872c2cae74b0ca0856610d5cccd
Laurent G <laurentguinotte@gmail.com>
2018-03-04 15:15:09 +0100

    Mass commit - see description
    New contact page, user-status

--- contacts.rb
+++ contacts.rb
@@ -5,11 +5,31 @@ require 'bcrypt'
 require 'yaml'
 require 'tilt/erubis'
 
+######### Configuration ####################
+
 configure do
   enable :sessions
   set :session_secret, 'secret'
   set :erb, escape_html: true
 end
+
+######### View Helpers #####################
+
+helpers do
+  def display_user_status(session)
+    if session[:username]
+      @username = session[:username]
+      erb :logged_in_infos
+    else
+      erb :logged_out_infos
+    end
+  end
+end
+
+######### Route Helpers ####################
+
+
+
 
 # Home page
 get '/' do
@@ -19,22 +39,22 @@ end
 
 # Display all contacts
 get '/contacts' do
-
+  erb :contacts, layout: :layout
 end
 
 # Add a new contact page
 get '/contacts/new' do
-
+  erb :new_contact, layout: :layout
 end
 
 # Display categories
 get '/categories' do
-  
+  erb :categories, layout: :layout
 end
 
 # Default route if route does not exists
 not_found do
-  session[:message] = "The page you requested does not exists."
+  session[:error] = "The page you requested does not exists."
   redirect '/'
 end
 
